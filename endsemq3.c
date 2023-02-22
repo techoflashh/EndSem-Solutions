00ef81a25c94270ebcc9d78e0ada8ba8b3b6975d
Himanshu Yadav <71442507+techoflashh@users.noreply.github.com>
2023-02-22 19:07:55 +0530

    Question 3 Solution

---
+++ endsemq3.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void permute(int n,int k,int len,int pos,int* comb,int arr[n]){
+    if (len==k){
+        for (int i=0;i<k;i++){
+            printf("%d ",comb[i]);
+        }
+        printf("\n");
+    }
+    else if (pos>=k || len>k){
+        return;
+    }
+    else{
+        for (int i=0;i<n;i++){
+            comb[len] = arr[i];
+            permute(n,k,len+1,pos+1,comb,arr);
+        }
+    }
+}
+
+int main(){
+    int n,k;
+    scanf("%d %d",&n,&k);
+    int arr[n];
+    int comb[k];
+    for (int i=0;i<n;i++){
+        arr[i] = i+1;
+    }
+    permute(n,k,0,0,comb,arr);
+    return 0;
+}

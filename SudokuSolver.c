43ee8676c42ea05542e134bbb6fb6335dc369d93
techoflashh <hy815088@gmail.com>
2023-02-23 09:18:10 +0530

    sudoku solver added

---
+++ SudokuSolver.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+
+int COUNT=0;
+bool check(char grid[4][4],int row,int coulumn,char e){
+    for (int i=0;i<4;i++){
+        if (grid[row][i]==e){
+            return false;
+        }
+        if (grid[i][coulumn]==e){
+            return false;
+        }
+        if (grid[2*(row/2) + i/2][2*(coulumn/2) + i%2]==e){
+            return false;
+        }
+    }   
+    return true;
+}
+
+bool solutions(char grid[4][4],char* arr){
+    for (int row=0;row<4;row++){
+        for (int col=0;col<4;col++){
+            if (grid[row][col]=='*'){
+                for (int i=0;i<4;i++){
+                    if (check(grid,row,col,arr[i])){
+                        grid[row][col]=arr[i];
+                        bool temp = solutions(grid,arr);
+                        if (temp==true){
+                            // COUNT++;
+                            return true;
+                        }
+                        else{
+                            grid[row][col]='*';
+                        }
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    // COUNT++;
+    return true;
+}
+int main(){
+    char grid[4][4];
+    for (int i=0;i<4;i++){
+        for (int j=0;j<4;j++){
+            scanf(" %c",&grid[i][j]);
+        }
+    }
+    char arr[4] = {'1','2','3','4'};
+    // solutions(grid,arr);
+    if (solutions(grid,arr)){
+        for (int i=0;i<4;i++){
+            for (int j=0;j<4;j++){
+                printf("%c ",grid[i][j]);
+            }
+            printf("\n");
+        }
+    }
+    printf("%d\n",COUNT);
+}
